8eb04774558e0f4a8f4ebfa63e84795ff0c91de6
NICOLAS LIMA DA SILVA <nicolas.lsilva5@senacsp.edu.br>
2025-01-14 15:24:34 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("oi sou nico");
+Console.WriteLine("estou aprendendo lógica de programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("olá");
+Console.WriteLine("meu nome é nicolas");
+Console.WriteLine("estou estando lógica de programação no senac largotreze");
