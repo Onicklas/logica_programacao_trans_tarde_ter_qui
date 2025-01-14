4a186063a1e92c26b98884802238b2d160725891
NICOLAS LIMA DA SILVA <nicolas.lsilva5@senacsp.edu.br>
2025-01-14 17:14:54 -0300

    ex06

--- ex05/obj/Debug/net8.0/ex05.AssemblyInfo.cs
+++ ex05/obj/Debug/net8.0/ex05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16198be7edd02836beb0eca61f0225285b9980ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d9bf25e18dc177e732caee9e691da8b845a938")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ex06/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Número1: ");
+int Num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Número2: ");
+int Num2 = int.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("+ : " + (Num1 + Num2));
+Console.WriteLine("- : " + (Num1 - Num2));
+Console.WriteLine("* : " + (Num1 * Num2));
+if (Num1 != 0)
+{
+    Console.WriteLine("/ : " + (Num1 / Num2));
+}
+else
+{
+    Console.WriteLine("/ : DIVISÃO POR ZERO NÃO É PERMITIDA!");
+}
+Console.WriteLine("======================");
+Console.WriteLine("Números são iguas?" + (Num1 == Num2));
+Console.WriteLine("Número 1 é maior que o 2? " + (Num1 > Num2));
+Console.WriteLine("Número 1 é menor que o 2? " + (Num1  < Num2));
+Console.WriteLine("Número 1 é maior igual o 2? " + (Num1 >= Num2));
+Console.WriteLine("Número 1 é maior igual o 2? " + (Num1 <= Num2));
