03d9bf25e18dc177e732caee9e691da8b845a938
NICOLAS LIMA DA SILVA <nicolas.lsilva5@senacsp.edu.br>
2025-01-14 17:00:39 -0300

    ex05

---
+++ ex05/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Numero1: ");
+int Numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Numero2: ");
+int Numero2 = int.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("========================");
+Console.WriteLine("=====SOMA DE NUMEROS====");
+Console.WriteLine("========================");
+Console.WriteLine($"A soma é: {Numero1 + Numero2} ");
